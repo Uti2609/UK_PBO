07ce2f2729e76fb2a7cdd52ddd74a110e9a951d9
Uti2609 <justiciauti@gmail.com>
2024-10-24 19:38:32 +0700

    MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -1,27 +1,26 @@
 public class MainClass {
-    // Polimorfisme: Method menerima parameter dari kelas induk
     public static void tampilkanSuaraHewan(Hewan hewan) {
         hewan.suara();  // Polimorfisme terjadi di sini
     }
 
     public static void main(String[] args) {
-        // Membuat objek dari kelas Anjing, Kucing, Harimau
-        Anjing anjing = new Anjing("Buddy", 5, "Golden Retriever");
+        // Membuat objek dari kelas Ayam, Kucing, Harimau
+        Ayam ayam = new Ayam("Jago", 2, "Ayam Kampung");
         Kucing kucing = new Kucing("Molly", 3, "Hitam");
         Harimau harimau = new Harimau("Shera", 7, 1000);
 
         // Memanggil method menggunakan polimorfisme
-        tampilkanSuaraHewan(anjing);
+        tampilkanSuaraHewan(ayam);
         tampilkanSuaraHewan(kucing);
         tampilkanSuaraHewan(harimau);
 
         // Memanfaatkan getter untuk mengambil nilai yang di-enkapsulasi
-        System.out.println("Anjing ras: " + anjing.getRas());
+        System.out.println("Ayam jenis: " + ayam.getJenisAyam());
         System.out.println("Kucing warna: " + kucing.getWarna());
         System.out.println("Harimau kekuatan cakar: " + harimau.getKekuatanCakar());
 
         // Perusahaan (kelas induk 2)
-        Perusahaan perusahaan = new Perusahaan("ABC Pet Shop");
+        Perusahaan perusahaan = new Perusahaan("Kebun Binatang");
         perusahaan.infoPerusahaan();
     }
 }
